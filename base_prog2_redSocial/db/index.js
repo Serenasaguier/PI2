3a1564f47bc2784eb3468a9fd57cb5863077e652
Serenasaguier <ssaguier@udesa.edu.ar>
2022-09-15 09:08:46 -0300

    mas fotos

--- base_prog2_redSocial/db/index.js
+++ base_prog2_redSocial/db/index.js
@@ -58,7 +58,28 @@ const index = {
                 autor: 'pepita_jr',
                 fotoPerfil: '/images/pepatwo.jpeg'
             },{
-                id_posteo: 2,
+                id_posteo: 1,
+                comentario: 'Que copado',
+                autor: 'mayo.nesa',
+                fotoPerfil: '/images/mayoone.jpeg'
+            },
+        ]
+        },
+        {
+            id: 6,
+            id_usuario: 5,
+            imagen: '/images/quinto3.jpeg',
+            caption: 'En el parque',
+            nombreUsuario: 'quinto5',
+            fotoPerfil: '/images/quinto1.jpeg',
+            comentarios : [ 
+                {
+                id_posteo: 6,
+                comentario: 'Me encanta esta foto',
+                autor: 'pepita_jr',
+                fotoPerfil: '/images/pepatwo.jpeg'
+            },{
+                id_posteo: 6,
                 comentario: 'Que copado',
                 autor: 'mayo.nesa',
                 fotoPerfil: '/images/mayoone.jpeg'
@@ -73,12 +94,34 @@ const index = {
             fotoPerfil: '/images/mayoone.jpeg',
             comentarios : [ 
                 {
-                id_posteo: 1,
+                id_posteo: 2,
                 comentario: 'Divino!',
                 autor: 'quinto5',
                 fotoPerfil: '/images/quinto1.jpeg'
             },{
                 id_posteo: 2,
+                comentario: 'Donde estas?',
+                autor: 'tamakito',
+                fotoPerfil: '/images/gatoone.jpeg'
+            },
+        ]
+
+        },
+        {
+            id: 7,
+            id_usuario: 3,
+            imagen: '/images/mayo3.png',
+            caption: 'primer plano', 
+            nombreUsuario: 'mayo.nesa',
+            fotoPerfil: '/images/mayoone.jpeg',
+            comentarios : [ 
+                {
+                id_posteo: 7,
+                comentario: 'Divino!',
+                autor: 'quinto5',
+                fotoPerfil: '/images/quinto1.jpeg'
+            },{
+                id_posteo: 7,
                 comentario: 'Donde estas?',
                 autor: 'tamakito',
                 fotoPerfil: '/images/gatoone.jpeg'
@@ -94,12 +137,32 @@ const index = {
             fotoPerfil: '/images/pepaone.jpeg',
             comentarios : [ 
                 {
-                id_posteo: 1,
+                id_posteo: 3,
                 comentario: 'Wachi :)',
                 autor: 'toroloco',
                 fotoPerfil: '/images/canicheone.jpeg'
             },{
-                id_posteo: 2,
+                id_posteo: 3,
+                comentario: 'Que locura!',
+                autor: 'mayo.nesa',
+                fotoPerfil: '/images/mayoone.jpeg'
+            },
+        ]
+        },{
+            id: 8,
+            id_usuario: 4,
+            imagen: '/images/pepa3.jpeg',
+            caption: 'jajaja rockstar',
+            nombreUsuario: 'pepita_jr',
+            fotoPerfil: '/images/pepaone.jpeg',
+            comentarios : [ 
+                {
+                id_posteo: 8,
+                comentario: 'Wachi :)',
+                autor: 'toroloco',
+                fotoPerfil: '/images/canicheone.jpeg'
+            },{
+                id_posteo: 8,
                 comentario: 'Que locura!',
                 autor: 'mayo.nesa',
                 fotoPerfil: '/images/mayoone.jpeg'
@@ -114,12 +177,32 @@ const index = {
             fotoPerfil: '/images/gatoone.jpeg',
             comentarios : [ 
                 {
-                id_posteo: 1,
+                id_posteo: 4,
                 comentario: 'Me encanta esta !!',
                 autor: 'pepita_jr',
                 fotoPerfil: '/images/pepaone.jpeg'
             },{
-                id_posteo: 2,
+                id_posteo: 4,
+                comentario: ' nada mas lindo',
+                autor: 'quinto5',
+                fotoPerfil: '/images/quinto1.jpeg'
+            },
+        ]
+        },{
+            id: 9,
+            id_usuario: 2,
+            imagen: '/images/tamakito2.jpeg',
+            caption: 'mirenme !!!', 
+            nombreUsuario: 'tamakito',
+            fotoPerfil: '/images/gatoone.jpeg',
+            comentarios : [ 
+                {
+                id_posteo: 9,
+                comentario: 'Me encanta esta !!',
+                autor: 'pepita_jr',
+                fotoPerfil: '/images/pepaone.jpeg'
+            },{
+                id_posteo: 9,
                 comentario: ' nada mas lindo',
                 autor: 'quinto5',
                 fotoPerfil: '/images/quinto1.jpeg'
@@ -134,16 +217,38 @@ const index = {
             fotoPerfil: '/images/canicheone.jpeg',
             comentarios : [ 
                 {
-                id_posteo: 1,
+                id_posteo: 5,
                 comentario: 'Lovely <3',
                 autor: 'pepita_jr',
                 fotoPerfil: '/images/pepaone.jpeg'
             },{
-                id_posteo: 2,
+                id_posteo: 5,
                 comentario: 'Faaa',
                 autor: 'quinto5',
                 fotoPerfil: '/images/quinto1.jpeg'
             },
+            
+        ]
+        },{
+            id: 10,
+            id_usuario: 1,
+            imagen: '/images/toro3.jpeg',
+            caption: 'fotooooo', 
+            nombreUsuario: 'toroloco',
+            fotoPerfil: '/images/canicheone.jpeg',
+            comentarios : [ 
+                {
+                id_posteo: 10,
+                comentario: 'Lovely <3',
+                autor: 'pepita_jr',
+                fotoPerfil: '/images/pepaone.jpeg'
+            },{
+                id_posteo: 10,
+                comentario: 'Faaa',
+                autor: 'quinto5',
+                fotoPerfil: '/images/quinto1.jpeg'
+            },
+            
         ]
         }
     ],
@@ -204,7 +309,8 @@ const index = {
             comentario: 'Que copado',
             autor: 'mayo.nesa',
             fotoPerfil: '/images/mayoone.jpeg'
-        }
+        },
+        
         
 
     ]
