3a03e25306c2de0c4ebe94cf163f734cd131d2f3
joacobrandan <jbrandanquarto@udesa.edu.ar>
2022-11-10 09:42:38 -0300

    arreglos

--- base_prog2_redSocial/database/config/config.js
+++ base_prog2_redSocial/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": "null",
     "database": "pets_db", // poner el nombre de nuestra base de datos
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
   }
 }
